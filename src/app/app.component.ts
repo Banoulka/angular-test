e05166ea3f0ba8099156453994f8de135c8b4aed
Banoulka <nathanbammy@hotmail.co.uk>
2024-06-21 07:32:42 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { QuoteListComponent } from './quote-list/quote-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, QuoteListComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'my-angular-app';
